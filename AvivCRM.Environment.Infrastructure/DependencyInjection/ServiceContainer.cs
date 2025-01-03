2486fc447c0a7dfa6d3f46b32a48df748ea180f4
Sivan T <Sivan T@DESKTOP-6PN6KNF>
2025-01-03 11:42:51 +0530

    validation messages renamed.

--- AvivCRM.Environment.Infrastructure/DependencyInjection/ServiceContainer.cs
+++ AvivCRM.Environment.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -16,18 +16,7 @@ public static class ServiceContainer
         services.AddDbContext<EnvironmentDbContext>(options =>
         options.UseSqlServer(configuration.GetConnectionString("environmentCS")));
 
-        //services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-        //services.AddScoped<ILeadAgentService, LeadAgentService>();
-        //services.AddScoped<ILeadCategoryService, LeadCategoryservice>();
-        //services.AddScoped<IClientService, ClientService>();
-        //services.AddScoped<ILeadSourceService, LeadSourceService>();
-        //services.AddScoped<ILeadStatusService, LeadStatusService>();
-        //services.AddScoped<IStateService, StateService>();
-        //services.AddScoped<ICityService, CityService>();
-
-
         services.AddScoped<IUnitOfWork, UnitOfWork>();
-        //services.AddScoped<ILeadCategory, LeadCategoryRepository>();
         services.AddScoped<ILeadSource, LeadSourceRepository>();
 
         return services;
