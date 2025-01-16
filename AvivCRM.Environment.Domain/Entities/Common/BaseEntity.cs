df09f219dc242323a66b065c0d3666a732054aee
Kansheyam V <kansheyam0812@gmail.com>
2025-01-16 11:01:17 +0530

    Client validation key updated

--- AvivCRM.Environment.Domain/Entities/Common/BaseEntity.cs
+++ AvivCRM.Environment.Domain/Entities/Common/BaseEntity.cs
@@ -1,6 +1,9 @@
-﻿namespace AvivCRM.Environment.Domain.Entities.Common;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvivCRM.Environment.Domain.Entities.Common;
 public abstract class BaseEntity
 {
+    [Key]
     public Guid Id { get; set; }
     public DateTime? CreatedOn { get; set; } = DateTime.UtcNow;
     public DateTime? ModifiedOn { get; set; }
---
+++ AvivCRM.Environment.Infrastructure/Persistence/Data/Migrations/20250115105127_Kansheyam-Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AvivCRM.Environment.Infrastructure.Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class KansheyamTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedOn",
+                schema: "Sivan",
+                table: "tblTask",
+                type: "datetime2",
+                nullable: true,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                schema: "Sivan",
+                table: "tblTask",
+                type: "datetime2",
+                nullable: true,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedOn",
+                schema: "Sivan",
+                table: "tblTask",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                schema: "Sivan",
+                table: "tblTask",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValueSql: "GETUTCDATE()");
+        }
+    }
+}
